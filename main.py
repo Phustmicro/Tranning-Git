a9a0df1774e03d5fbd23842d500f81f1eaa846c1
phu phu <Phustmicro>
2023-04-20 22:53:18 +0200

    Check pull request

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Person import Person
 
 import picoscope as pc
 def main():
-    obj = Person("Le Quang Phu", 38)
+    obj = Person("Le Quang Phu, pr-branch", 38)
     obj.greet()
 if __name__ == "__main__":
     main()
