4df3b3815c39a73b4ac3277240de2aeaed0b032f
phu phu <Phustmicro>
2023-04-20 22:18:46 +0200

    test conflict 3

--- main.py
+++ main.py
@@ -2,12 +2,7 @@
 from Person import Person
 import picoscope as pc
 def main():
-<<<<<<< HEAD
     obj = Person("Le Quang Phu, asdasd")
-=======
-    obj = Person("abc xyz")
->>>>>>> refs/remotes/origin/master
     obj.greet()
-
 if __name__ == "__main__":
     main()
